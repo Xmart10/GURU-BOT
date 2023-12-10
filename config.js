fde4a99b5b225b3c2ddc1aaa20cc5fffbffacec6
Xmart10 <153458652+Xmart10@users.noreply.github.com>
2023-12-10 21:12:27 +0100

    Update config.js

--- config.js
+++ config.js
@@ -7,7 +7,7 @@ import axios from 'axios'
 
 
 global.owner = [
-  ['917605902011', 'GURU', true],
+  ['7010198005', 'Xmart', true],
   ['6281255369012', 'Ash', true], 
   [''] 
 ] //Number of owners
